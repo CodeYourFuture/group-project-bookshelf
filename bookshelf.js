6cc15aea3212808d0faf7387f20183defe75fd80
yevhensydorov <assid@ukr.net>
2017-11-29 10:27:46 +0000

    organising bookshelf 3 paragraphs done

--- bookshelf.js
+++ bookshelf.js
@@ -18,13 +18,27 @@ function fetchBooks() {
 
 function processBooks(booksJSON) {
 	var ulTag = document.createElement("ul"); // create <ul> Tag
+
 	// This forEach loop creates several li tags
 	booksJSON.forEach(function(el){
 		var newLiElement = document.createElement("li"); // create <li> element
 		newLiElement.setAttribute("id", el.id); // set id attribute to each <li>
-		newLiElement.innerHTML = "-" + " " + el.title; // create el number of li element with text that get from bookJSON
+		
+		var upButton = document.createElement("button"); //
+		var upButtonText = document.createTextNode("⬆"); // CREATE BUTTONS WITH ARROWS UP
+		upButton.appendChild(upButtonText);							//
+
+		var downButton = document.createElement("button"); //
+		var downButtonText = document.createTextNode("⬇"); // CREATE BUTTONS WITH ARROWS DOWN
+		downButton.appendChild(downButtonText);						//
+		
+		newLiElement.appendChild(upButton); // append up button to <li> element
+		newLiElement.appendChild(downButton); // append down button to <li> element
+		var liText = document.createTextNode(" - " + el.title); // create text in the <li> from JSON
+		newLiElement.appendChild(liText); // apppend text to the <li>
+
 		ulTag.appendChild(newLiElement); // append <li> to <ul>
-	})
+	});
 
 	document.body.appendChild(ulTag); // append <ul> to the body
 }
@@ -33,4 +47,12 @@ function removeBtn() {
 	var button = document.getElementById("fetch-books-btn"); // get the button with "fetch-books-btn" id
 	button.removeEventListener("click", fetchBooks);
 	button.remove();
+}
+
+function moveUp(id) {
+	console.log("Move up" + " " + id);
+}
+
+function moveDown(id) {
+	console.log("Move down" + " " + id);
 }
