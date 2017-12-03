1a2126bdfc17872606f044035e38638e705a9860
Moradi66 <mohsen000069@gmail.com>
2017-12-03 18:37:33 +0000

    done

--- bookshelf.js
+++ bookshelf.js
@@ -1,14 +1,21 @@
 window.bookshelf = (function() {
-        var btn = document.createElement("button")
-        btn.setAttribute('id', 'fetch-books-btn')
-        var text = document.createTextNode('Fetch Books')
-        btn.appendChild(text)
-        btn.addEventListener('click', fetchBooks)
-        document.body.appendChild(btn)
+        function createBtn(){
+                var btn = document.createElement("button")
+                btn.setAttribute('id', 'fetch-books-btn')
+                var text = document.createTextNode('Fetch Books')
+                btn.appendChild(text)
+                btn.addEventListener('click', fetchBooks)
+                document.body.appendChild(btn)
+        }
+        
         
         function fetchBooks() {
                 const booksJSON = 'https://raw.githubusercontent.com/codeyourfuture/bookshelf-project/master/books.json'
-                fetch(booksJSON).then(response => response.json()).then(processBooks).then(removeBtn)
+                fetch(booksJSON)
+                .then(response => response
+                        .json())
+                        .then(processBooks)
+                        .then(removeBtn)
         }
         
         function processBooks(books) {
@@ -20,47 +27,63 @@ window.bookshelf = (function() {
                         btnup.setAttribute('id', 'moveUp')
                         var btnuptextNode = document.createTextNode('⬆')
                         btnup.appendChild(btnuptextNode)
-                        btnup.addEventListener('click', moveUp)
+                        
                         liNode.appendChild(btnup)
         
                         var btndown = document.createElement('button')
                         btndown.setAttribute('id', 'moveDown')
                         var btndowntextNode = document.createTextNode('⬇')
                         btndown.appendChild(btndowntextNode)
-                        btndown.addEventListener('click', moveDown)
+                        
                         liNode.appendChild(btndown)
                         var textNode = document.createTextNode(' - ' + book.title + ' by ' + book.author)
                         liNode.appendChild(textNode)
                         listNode.appendChild(liNode)
         
-                        function moveUp() {
-                                var x = document.getElementById("moveUp")
-                                liNode.addEventListener('click', function(event) {
-                                        var parent = event.target.parentNode;
-                                        listNode.insertBefore(parent, listNode.childNodes[0]);   
-                                })
-                        }
-        
-                        function moveDown() {
-                                var xd = document.getElementById("moveDown")
-                                liNode.addEventListener('click', function(event) {
-                                        var parent = event.target.parentNode;
-                                        listNode.insertBefore(parent, listNode.childNodes.nextSibling);
-                                })
-                        }
+                       
                 })
+                listNode.addEventListener('click', function(event){
+
+                             if (event.target.innerHTML === '⬆') {
+                                        moveUp(event.target.parentNode.id)
+                            } else if (event.target.innerHTML=== '⬇') {
+                                        moveDown(event.target.parentNode.id)
+                            }
+                })
+
                 document.body.appendChild(listNode)
+
         }
-        
+
+        function moveUp(id) {
+                const node = document.getElementById(id)
+                const prev = node.previousSibling
+                if (prev === null) return undefined
+                const parent = node.parentNode
+                const detachedNode = parent.removeChild(node)
+                parent.insertBefore(detachedNode, prev)
+        }
+
+        function moveDown(id) {
+                const node = document.getElementById(id)
+                const next = node.nextSibling
+                if (next === null) return undefined
+                const parent = node.parentNode
+                const detachedNode = parent.removeChild(node)
+                parent.insertBefore(detachedNode, next.nextSibling);
+        }
+
         function removeBtn() {
                 var btn = document.getElementById('fetch-books-btn')
                 btn.removeEventListener('click', fetchBooks)
                 btn.remove()
         } 
-        return {
-                init: createBtn
+            return {
+                init: createBtn,
+                removeBtn,
+                fetchBooks,
+                processBooks,
+                moveUp,
+                moveDown
             }
     }())
-
-
-    
