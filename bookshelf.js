81b3a215be06a2bc1ca9c459b880d56b8d83cb39
Viktor Protsenko <protsenko.viktor@gmail.com>
2018-02-09 19:38:08 +0000

    second commit

--- bookshelf.js
+++ bookshelf.js
@@ -1,6 +1,25 @@
-var button = document.createElement('btn');
+var button = document.createElement('button');
 button.setAttribute('id', 'fetch-books-btn');
 var text = document.createTextNode('Fetch Books');
 button.appendChild(text);
-button.addEventListener('click', fetchBooks, true);
-body.appendChild(button);
+button.addEventListener('click', fetchBooks);
+document.body.appendChild(button);
+
+function fetchBooks() {
+    const booksJSON = 'https://raw.githubusercontent.com/codeyourfuture/bookshelf-project/master/books.json'
+    fetch(booksJSON)
+        .then(response => response.json())
+        .then(processBooks)
+};
+
+function processBooks(booksJSON) {
+var ul = document.createElement('ul');
+booksJSON.forEach(function(item) {
+    var li = document.createElement("li");
+    li.setAttribute('id', item['id']);
+    var textNode = document.createTextNode(item['title'] + ' by ' + item['author'])
+    li.appendChild(textNode);
+    ul.appendChild(li);
+});
+document.body.appendChild(ul);
+};
