490f24cabc00bbc8c6425002c698228e59224121
Viktor Protsenko <protsenko.viktor@gmail.com>
2018-02-10 01:10:02 +0000

    end

--- bookshelf.js
+++ bookshelf.js
@@ -15,13 +15,23 @@ function fetchBooks() {
 
 function processBooks(booksJSON) {
     var ul = document.createElement('ul');
+    ul.setAttribute('id', 'list');
     booksJSON.forEach(function (item) {
         var li = document.createElement("li");
         li.setAttribute('id', item['id']);
-        var textNode = document.createTextNode(item['title'] + ' by ' + item['author'])
+        var upBtn = document.createElement("button");
+        var textUpBtn = document.createTextNode("▲");
+        upBtn.appendChild(textUpBtn);
+        li.appendChild(upBtn);
+        var downBtn = document.createElement("button");
+        var textDownBtn = document.createTextNode("▼");
+        downBtn.appendChild(textDownBtn);
+        li.appendChild(downBtn);
+        var textNode = document.createTextNode(' - ' + item['title'] + ' by ' + item['author']);
         li.appendChild(textNode);
         ul.appendChild(li);
     });
+    ul.addEventListener("click", InlineFunction);
     document.body.appendChild(ul);
 };
 
@@ -30,3 +40,36 @@ function removeBtn() {
     btn.removeEventListener('click', fetchBooks);
     btn.remove();
 };
+
+function moveUp(id) {
+    if (id === '1') { return }
+    var node = document.getElementById('list');
+    var target = document.getElementById(id);
+    var newNode = +id;
+    newNode = newNode - 1;
+    newNode = newNode + '';
+    var before = document.getElementById(newNode);
+    node.insertBefore(target, before);
+};
+
+function moveDown(id) {
+    if (id === '5') { return }
+    var node = document.getElementById('list');
+    var target = document.getElementById(id);
+    var newNode = +id;
+    newNode = newNode + 1;
+    newNode = newNode + '';
+    var before = document.getElementById(newNode);
+    node.insertBefore(target, before);
+};
+
+function InlineFunction(event) {
+    var findId = event.target.parentElement.id;
+    var findText = event.target.textContent;
+    if (findText === "▲") {
+        moveUp(findId);
+    } else {
+        moveDown(findId);
+    };
+};
+
