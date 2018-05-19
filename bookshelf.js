da5ac8328afb7e78eb6e67ae9c92770b3ec56d6d
NawalC <you@example.comnawal.careem@yahoo.com>
2018-05-19 15:23:11 +0100

    added Wiring up the button & Fetching the books data

--- bookshelf.js
+++ bookshelf.js
@@ -7,15 +7,15 @@ button1.addEventListener ("click" , fetchBooks)             //Add event handler
 var body = document.getElementsByTagName("body")               // <body><button>id = "fetch-books-btn"</button></body>
 document.body.appendChild(button1);                         // Append <button> to <body>
 
-function addElement(){
-	var parentUl = document.createElement('ul');  // parent document
-	var textForList = document.createTextNode('Hello World') //child document
-	parentUl.appendChild(textForList);
-	var body = document.getElementsByTagName("body")              
-document.body.appendChild(parentUl);  
-	}       
+// function addElement(){
+// 	var parentUl = document.createElement('ul');  // parent document
+// 	var textForList = document.createTextNode('Hello World') //child document
+// 	parentUl.appendChild(textForList);
+// 	var body = document.getElementsByTagName("body")              
+// document.body.appendChild(parentUl);  
+// 	}       
 
-addElement();
+// addElement();
 
 
 function fetchBooks(){
@@ -26,35 +26,40 @@ fetch(booksJSON)
     .then(books => {
 		/* TODO: create the bookshelf */
 				
-        var names = ['title','author'];
-		var ul = document.createElement('ul');
+			   var createAulTag = document.createElement('ul');
+			              
+			   document.body.appendChild(createAulTag);
+
+			//    var createAliTag =document.createElement('li');
+			//    createAulTag.getElementsByTagName("ul")              
+			//    createAulTag.appendChild(createAliTag);
+			   
+
 		//document.getElementById('1').appendChild(ul);
  
-		names.forEach(function(name){
+		books.forEach(function(book){
+			
 			var li = document.createElement('li');
-			ul.appendChild(li);
-			li.innerHTML += name;
-			console.log(name)
-		});
-        console.log(books)
-    })
+			li.setAttribute('id', book.id);
+			createAulTag.appendChild(li);
+			li.innerHTML += book.title + ' by ' +  book.author;
+			console.log(book)
+		})
+		
+     
+		console.log(books)
+		
+	 })
+	 .then(removeBtn());
+	
+}
+function removeBtn(){
 
+		
+	var button1 = document.getElementById("fetch-books-btn")
+	document.getElementById("fetch-books-btn").style.visibility="hidden";
+	button1.removeEventListener ("click", fetchBooks) 
+	
+	
 }
 
-// Write your solution inside the `processBooks` function. The function should create an unordered list containing list items with the author and titles of the books. Each list item should also have an id attribute set to the book id.
-
-// When you click the `Fetch Books` button it should add the following HTML to the webpage.
-
-// ```html
-// <ul>
-//     <li id="1">The Catcher in the Rye by J. D. Salinger</li>
-//     <li id="2">To Kill a Mockingbird by Harper Lee</li>
-//     <li id="3">The Grapes of Wrath by John Steinbeck</li>
-//     <li id="4">The Great Gatsby by F Scott Fitzgerald</li>
-//     <li id="5">Moby-Dick by Herman Melville</li>
-// </ul>
-// ```
-
-// ### Hint
-
-// Use the [`forEach`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach) method to iterate over the books array and create the list items.
