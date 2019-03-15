f64aa8153d7c38132890ff9131cc72609078541e
Ivina Pontes <ivinapontes@outlook.com>
2019-03-15 21:40:16 +0000

    started organising the bookshelf

--- bookshelf.js
+++ bookshelf.js
@@ -1,9 +1,9 @@
 //Create and append button to the html
 var btn = document.createElement("button");
-btn.setAttribute("id", "fetch-books-btn");
+    btn.setAttribute("id", "fetch-books-btn");
 var txt = document.createTextNode("Fetch Books");
-btn.appendChild(txt);
-btn.addEventListener("click", fetchBooks);
+    btn.appendChild(txt);
+    btn.addEventListener("click", fetchBooks);
 document.body.appendChild(btn);
 
 function fetchBooks() {
@@ -19,16 +19,16 @@ function fetchBooks() {
 function processBooks(books) {
   var ul = document.createElement("ul");
   books.forEach(function(book) {
-      let bookList = `<li><button>⬆</button>
+    let bookList = `<li><button>⬆</button>
     <button>⬇</button> - ${book.title} by ${book.author}</li><br/>`;
-    ul.innerHTML += bookList;
+        ul.innerHTML += bookList;
     document.body.appendChild(ul);
   });
 }
 
 function removeBtn() {
     var findBtn = document.getElementById("fetch-books-btn");
-    findBtn.removeEventListener("click", processBooks);
+        findBtn.removeEventListener("click", processBooks);
     document.body.removeChild(findBtn);
 }
 
