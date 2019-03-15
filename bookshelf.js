8a4ebdd4aef77f651eb907085c48f53f9a14e717
Ivina Pontes <ivinapontes@outlook.com>
2019-03-15 21:39:15 +0000

    started organising the bookshelf

--- bookshelf.js
+++ bookshelf.js
@@ -11,15 +11,31 @@ function fetchBooks() {
     "https://raw.githubusercontent.com/codeyourfuture/bookshelf-project/master/books.json";
   fetch(booksJSON)
     .then(response => response.json())
-    .then(processBooks);
+    .then(processBooks)
+    .then(removeBtn);
 }
 //Create unordered list
 
 function processBooks(books) {
   var ul = document.createElement("ul");
   books.forEach(function(book) {
-    let bookList = ` ${book.title} by ${book.author}</li><br/>`;
+      let bookList = `<li><button>⬆</button>
+    <button>⬇</button> - ${book.title} by ${book.author}</li><br/>`;
     ul.innerHTML += bookList;
     document.body.appendChild(ul);
   });
+}
+
+function removeBtn() {
+    var findBtn = document.getElementById("fetch-books-btn");
+    findBtn.removeEventListener("click", processBooks);
+    document.body.removeChild(findBtn);
+}
+
+function moveUp(id){
+    console.log("move up")
+}
+
+function moveDown(id){
+    console.log("move down")
 }
